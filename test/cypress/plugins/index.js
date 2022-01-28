6172ac7ef8fe9c601ebd5695d9b120035874d014
alphabiz-se4 <92558550+alphabiz-se4@users.noreply.github.com>
2022-01-28 15:48:36 +0800

    Try to run the test

---
+++ test/cypress/plugins/index.js
@@ -0,0 +1,53 @@
+/* eslint-env node */
+// ***********************************************************
+// This example plugins/index.js can be used to load plugins
+//
+// You can change the location of this file or turn off loading
+// the plugins file with the 'pluginsFile' configuration option.
+//
+// You can read more here:
+// https://on.cypress.io/plugins-guide
+// ***********************************************************
+
+// This function is called when a project is opened or re-opened (e.g. due to
+// the project's config changing)
+
+require('dotenv').config()
+const { mailListener, getSMS, sleep } = require('../../utils/getCode')
+
+module.exports = (on, config) => {
+  on('task', {
+    async getMail ({ type, time }) {
+      let res = await mailListener(type, time)
+      // 如果未获取到邮件，等待5秒后，重新调用mailListener
+      if (!res) {
+        await sleep(5000)
+        console.log('mailListener again')
+        res = await mailListener(type, time)
+      }
+      return res
+    },
+    async getSMS ({ type, time }) {
+      let res = await getSMS(type, time)
+      // 如果未获取到短信，等待10秒后，重新调用getSMS
+      if (!res) {
+        await sleep(10000)
+        console.log('getSMS again')
+        res = await getSMS(type, time)
+      }
+      console.log('code:' + res)
+      return res
+    }
+  })
+  // copy any needed variables from process.env to config.env
+  config.env.testEmail = process.env.EMAIL_USERNAME
+  config.env.test1Email = process.env.TEST1_EMAIL
+  config.env.test2Email = process.env.TEST2_EMAIL
+  config.env.test3Email = process.env.TEST3_EMAIL
+  config.env.test1PhoneNumber = process.env.TEST1_PHONE_NUMBER
+  config.env.test2PhoneNumber = process.env.TEST2_PHONE_NUMBER
+  config.env.password = process.env.TEST_PASSWORD
+  config.env.resetPassword = process.env.TEST_RESET_PASSWORD
+  // do not forget to return the changed config object!
+  return config
+}
---
+++ test/cypress/support/commands.js
@@ -0,0 +1,206 @@
+// ***********************************************
+// This example commands.js shows you how to
+// create various custom commands and overwrite
+// existing commands.
+//
+// For more comprehensive examples of custom
+// commands please read more here:
+// https://on.cypress.io/custom-commands
+// ***********************************************
+//
+//
+// -- This is a parent command --
+// Cypress.Commands.add("login", (email, password) => { ... })
+//
+//
+// -- This is a child command --
+// Cypress.Commands.add("drag", { prevSubject: 'element'}, (subject, options) => { ... })
+//
+//
+// -- This is a dual command --
+// Cypress.Commands.add("dismiss", { prevSubject: 'optional'}, (subject, options) => { ... })
+//
+//
+// -- This is will overwrite an existing command --
+// Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+import 'cypress-file-upload'
+Cypress.Commands.add(
+  'dataCy',
+  {
+    prevSubject: 'optional'
+  },
+  (subject, value) => {
+    return cy.get(`[data-cy=${value}]`, {
+      withinSubject: subject
+    })
+  }
+)
+
+Cypress.Commands.add('testRoute', (route) => {
+  cy.location().should((loc) => {
+    if (loc.hash.length > 0) {
+      // Vue-Router in hash mode
+      expect(loc.hash).to.contain(route)
+    } else {
+      // Vue-Router in history mode
+      expect(loc.pathname).to.contain(route)
+    }
+  })
+})
+
+// these two commands let you persist local storage between tests
+const LOCAL_STORAGE_MEMORY = {}
+
+Cypress.Commands.add('saveLocalStorage', () => {
+  Object.keys(localStorage).forEach((key) => {
+    LOCAL_STORAGE_MEMORY[key] = localStorage[key]
+  })
+})
+
+Cypress.Commands.add('restoreLocalStorage', () => {
+  Object.keys(LOCAL_STORAGE_MEMORY).forEach((key) => {
+    localStorage.setItem(key, LOCAL_STORAGE_MEMORY[key])
+  })
+})
+
+// jump page command
+Cypress.Commands.add('toSignIn', () => {
+  cy.get('[aria-label="Menu"]').click()
+  cy.get('.corner > .q-item').click()
+  // cy.location('pathname', { timeout: 10000 }).should('eq', '/account')
+  cy.get('.q-card').should('be.visible')
+})
+Cypress.Commands.add('toCredits', () => {
+  cy.get('[aria-label="Menu"]').click()
+  cy.get(':nth-child(4) > .q-item').click()
+  cy.get('.q-linear-progress__model', { timeout: 10000 }).should('be.visible')
+  cy.get('.q-linear-progress__model', { timeout: 10000 }).should('not.exist')
+})
+
+// cacheSession = true => call session()
+Cypress.Commands.add('signIn', (username, password, { cacheSession = true } = {}) => {
+  const login = () => {
+    cy.visit('/')
+    // toggle sign in page
+    cy.toSignIn()
+    // sign in start
+    let user
+    if (/@/.test(Cypress.env(username))) {
+      user = Cypress.env(username)
+    } else {
+      user = '+1' + Cypress.env(username)
+    }
+    cy.contains('Phone number or email').type('{selectall}{backspace}').type(user)
+    cy.contains('Password').type('{selectall}{backspace}').type(Cypress.env(password))
+    cy.get('.q-btn__content').contains('Sign in').click()
+    // wait page jump
+    cy.get('.q-notification__message', { timeout: 40000 }).should('be.visible').then($header => {
+      const text = $header.text()
+      cy.log(text)
+      if (/There is a problem with the network, please try again later/.test(text) ||
+        /Pending sign-in attempt already in progress/.test(text)) {
+        cy.log('text is There is a problem with the network')
+        cy.get('.q-notification__message', { timeout: 30000 }).should('not.be.visible')
+        cy.get('.q-btn__content').contains('Sign in').click()
+      } else {
+        cy.log('text is not network')
+      }
+    })
+    cy.get('.q-notification__message', { timeout: 30000 }).should('have.text', 'Signed in')
+    cy.contains('Lv.', { timeout: 12000 }).click()
+    cy.get('[data-cy=account-settings-btn]').click()
+    // cy.location('pathname', { timeout: 30000 }).should('eq', '/account/settings')
+    // sign in end
+  }
+  if (cacheSession) {
+    cy.session([username, password], login, {
+      validate () {
+        cy.visit('/')
+        cy.get('[aria-label="Menu"]').click()
+        cy.contains('Lv.', { timeout: 12000 }).click()
+        cy.get('[data-cy=account-settings-btn]').click()
+        // cy.location('pathname', { timeout: 12000 }).should('eq', '/account/settings')
+        cy.get('.account-setting__verification').contains(Cypress.env(username))
+      }
+    })
+    cy.visit('/')
+    cy.get('[aria-label="Menu"]').click()
+    cy.contains('Lv.', { timeout: 12000 }).click()
+    cy.get('[data-cy=account-settings-btn]').click()
+    // cy.location('pathname', { timeout: 12000 }).should('eq', '/account/settings')
+  } else {
+    login()
+  }
+})
+Cypress.Commands.add('signOut', () => {
+  // sign out start
+  cy.get('[aria-label="Menu"]').click()
+  cy.get('.corner > .q-item').click()
+  cy.get("[data-cy='sign-out-btn']").click()
+  // cy.intercept('https://cognito-idp.ca-central-1.amazonaws.com/').as('completed')
+  // cy.wait('@completed', { timeout: 10000 })
+  cy.contains('Signed out', { timeout: 30000 }).should('be.visible')
+  // sign out end
+})
+
+Cypress.Commands.add('getAccountStatus', () => {
+  cy.sleep(1000)
+  cy.get('.corner > .q-item').then(($el) => {
+    const isVisible = Cypress.dom.isVisible($el)
+    cy.log('isVisible' + isVisible)
+    if (!isVisible) {
+      cy.get('[aria-label="Menu"]').click()
+    } // true
+  })
+  cy.get('.corner > .q-item').then(($div) => {
+    const text = $div.text()
+    cy.log(text)
+    // cy.log(/Lv/.test(text))
+    expect(text).to.match(/Lv/)
+  })
+})
+
+Cypress.Commands.add('deleteUser', (password) => {
+  // delete user start
+  cy.get('.q-btn__wrapper > .q-btn__content').contains('delete account').click()
+  cy.get('.q-card', { timeout: 10000 }).should('be.visible').then($el => {
+    cy.get('[aria-label="Password"]').type('{selectall}{backspace}').type(Cypress.env(password))
+
+    cy.get("[data-cy='submit-btn']").click()
+  })
+  cy.location('pathname', { timeout: 15000 }).should('eq', '/')
+  // cy.contains('Want to Join?', { timeout: 12000 })
+  cy.get('.q-notification__message', { timeout: 10000 }).contains('Deleted')
+  cy.log('删除成功')
+  // delete user end
+})
+
+// credits command
+Cypress.Commands.add('transfer', (ID, amount) => {
+  // 转账 start
+  cy.get('button').contains('Transfer').click()
+  // cy.get('.q-card:nth-child(2) > .q-card__section:nth-child(1) > :nth-child(1)').click()
+  cy.get('.q-dialog__inner > .q-card', { timeout: 5000 }).should('be.visible').then($card => {
+    cy.get('[aria-label="Receipt Code"]').then($input => {
+      cy.log('111:' + $input.val())
+      if ($input.val() === '') cy.get('[aria-label="Receipt Code"]').type(ID)
+    })
+    cy.get('[aria-label="Transfer Amount"]').type(amount)
+    cy.get('.primary > .q-btn__wrapper').click()
+  })
+  // 等待 q-card 退出
+  cy.get('.q-dialog__inner > .q-card', { timeout: 20000 }).should('not.exist')
+  // 转账 end
+  cy.sleep(500)
+})
+
+// util command
+Cypress.Commands.add('sleep', (ms) => {
+  return new Promise(resolve => setTimeout(resolve, ms))
+})
+Cypress.Commands.add('times', (times, callback) => {
+  for (let n = 0; n < times; n++) {
+    cy.log('The' + n + 'cycle')
+    callback && callback()
+  }
+})
