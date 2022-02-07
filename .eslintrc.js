f1d330b3e03093315747d0e95d36b916eb52d2cc
alphabiz-se4 <92558550+alphabiz-se4@users.noreply.github.com>
2022-02-07 10:04:22 +0800

    add packager.js

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "commonjs": true,
+        "es2021": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:vue/essential"
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest"
+    },
+    "plugins": [
+        "vue"
+    ],
+    "rules": {
+    }
+}
