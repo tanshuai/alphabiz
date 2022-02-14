cb99bdc19fececa1a16ae2343e2f8d4a6876e302
alphabiz-se4 <92558550+alphabiz-se4@users.noreply.github.com>
2022-02-14 13:30:49 +0800

    add playwright test\try run playwright

---
+++ copyPatch.js
@@ -0,0 +1,28 @@
+// UnPackaged files uploaded by private repository, webTorrent module without patch after 'YARN' installation.
+// So you need to run this file, the root directory of modified webtorrent copied to build/electron/UnPackaged/node_modules, 
+// then run the playwright test, effective download function
+const fs = require('fs')
+const path = require('path')
+const copy = async () => {
+  ['webtorrent'].forEach(dep => {
+    const src = path.resolve(__dirname, 'node_modules', dep)
+    const dest = path.resolve(__dirname, 'build/electron/UnPackaged/node_modules', dep)
+    if (!fs.existsSync(src)) return
+    if (fs.existsSync(dest)) fs.rmSync(dest, { recursive: true })
+    const copyRecursive = (src, dest) => {
+      if (fs.statSync(src).isDirectory()) {
+        fs.readdirSync(src).forEach(dir => {
+          copyRecursive(path.resolve(src, dir), path.resolve(dest, dir))
+        })
+      } else {
+      // ensure directory exists
+        if (!fs.existsSync(path.dirname(dest))) {
+          fs.mkdirSync(path.dirname(dest), { recursive: true })
+        }
+        fs.copyFileSync(src, dest)
+      }
+    }
+    copyRecursive(src, dest)
+  })
+}
+copy()
---
+++ test/playwright/models/commands.js
@@ -0,0 +1,126 @@
+const { expect } = require('@playwright/test')
+class Commands {
+  constructor (page) {
+    this.page = page
+    // menu
+    this.menuIcon = page.locator('[aria-label="Menu"]')
+    this.homeLink = page.locator('text=HomeDownload/Upload and Media Files')
+    this.downloadingStatus = page.locator('text=Downloading (')
+    this.uploadingStatus = page.locator('text=Uploading (')
+    this.downloadedStatus = page.locator('text=Downloaded (')
+    this.playerLink = page.locator('text=Player Media Player')
+    this.creditsLink = page.locator('text=Credits Check Account Balance')
+    this.developmentLink = page.locator('text=developer_modeDevelopment Developer Mode for Internal Use')
+    this.accountLink = page.locator('.corner')
+    this.accountSettings = page.locator('[data-cy="account-settings-btn"]')
+    // home
+    this.downloadBtn = page.locator('button[role="button"]:has-text("Download")')
+    // credits
+    // account
+  }
+
+  /**
+   *
+   * @param {string} target - homeLink、playerLink、creditsLink、accountLink
+   */
+  async jumpPage (firstTarget, secondTarget) {
+    const menuButton = await this[secondTarget] || await this[firstTarget]
+    if (await this[firstTarget].isHidden()) {
+      await this.menuIcon.click()
+    }
+    if (secondTarget) {
+      if (await menuButton.isHidden()) {
+        await this[firstTarget].click()
+      }
+    }
+    await menuButton.click()
+  }
+
+  // home
+  async downloadTorrent (magnet) {
+    await this.downloadBtn.click()
+    await this.page.fill('input[type="text"]', magnet)
+    await this.page.fill('[aria-label="Download directory position"]', './test/download')
+    await this.page.click(':nth-match(button[role="button"]:has-text("Download"), 2)')
+  }
+
+  async signIn (username, password, isWaitAlert) {
+    if (await this.page.locator('[aria-label="Phone number or email"]').isHidden()) this.jumpPage('accountLink')
+    await this.page.waitForTimeout(500)
+    await this.page.fill('[aria-label="Phone number or email"]', username)
+    await this.page.fill('[aria-label="Password"]', password)
+    await this.page.click('button[role="button"]:has-text("Sign in")')
+
+    if (isWaitAlert) {
+      await this.page.locator('.q-notification__message >> text=Signed in').waitFor({ timeout: 40000 })
+    }
+  }
+
+  async signOut () {
+    await this.jumpPage('accountLink')
+    await this.page.click('text=Sign out')
+    await this.page.locator('.q-notification__message >> text=Signed out').waitFor()
+  }
+
+  async deleteUser (password) {
+    // delete user start
+    await this.page.click('button[role="button"]:has-text("delete account")')
+    await this.page.fill('[aria-label="Password"]', password)
+    await this.page.click('text=delete account >> ../..//Button[1]')
+    // delete user end
+    // 断言已删除的提示框弹出
+    await this.page.click('div[role="alert"]:has-text("check_circleDeleted")')
+  }
+
+  // credits
+
+  async getID () {
+    await this.page.click('.q-card:nth-child(2) > .q-card__section:nth-child(1) > :nth-child(2)')
+    let userID = (await this.page.locator('.col-grow > .q-btn > .q-btn__wrapper').innerText()).split('')
+    // 将收款人id中的大写转为小写
+    let newStr = ''
+    // 通过for循环遍历数组
+    for (let i = 0; i < userID.length; i++) {
+      if (userID[i] >= 'A' && userID[i] <= 'Z') { newStr += userID[i].toLowerCase() } else { newStr += userID[i] }
+    }
+    userID = newStr
+    await this.page.click('button[role="button"]:has-text("Cancel")')
+
+    return userID
+  }
+
+  async getCredit () {
+    await this.page.waitForTimeout(3000)
+    return await this.page.locator('.text-right > div').innerText()
+  }
+
+  async transfer (ID, amount) {
+    await this.page.click('.q-card:nth-child(2) > .q-card__section:nth-child(1) > :nth-child(1)')
+    await this.page.fill('[aria-label="Receipt Code"]', ID)
+    await this.page.fill('[aria-label="Transfer Amount"]', amount)
+    await this.page.click('.q-form >> button[role="button"]:has-text("TRANSFER")')
+    await this.page.locator(':nth-child(2) > .q-card').waitFor('hidden')
+  }
+
+  async checkBillDetail () {
+    await this.page.waitForLoadState()
+    await this.page.waitForTimeout(1000)
+    await this.page.click('.q-table__grid-content > :nth-child(1)')
+    await this.page.locator('form >> text=cancel').waitFor('visible')
+    for (var index in arguments) {
+      if (arguments[index] === 'Transfer') {
+        const categoryText = await this.page.locator('form >> text=Category >> //following::*[1]').innerText()
+        expect(categoryText).toBe(arguments[index])
+      } else if (arguments[index] === 'finish') {
+        const statusText = await this.page.locator('form >> text=Status >> //following::*[1]').innerText()
+        expect(statusText).toBe(arguments[index])
+      } else if (/^(\+|-)\d+$/.test(arguments[index])) {
+        const changedAmountText = await this.page.locator('form >> text=Changed Amount >> //following::*[1]').innerText()
+        expect(changedAmountText).toBe(arguments[index])
+      } else { await this.page.click('form >> :nth-match(div:has-text("' + arguments[index] + '"), 3)') }
+    }
+    await this.page.click('button[role="button"]:has-text("Cancel")')
+  }
+}
+
+module.exports = { Commands }
