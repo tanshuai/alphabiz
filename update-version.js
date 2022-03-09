7fbe9e831996fa80cdb595768d22f203050e5d5c
alphabiz-se4 <92558550+alphabiz-se4@users.noreply.github.com>
2022-03-09 10:58:03 +0800

    fix timezone、sha7 error

--- update-version.js
+++ update-version.js
@@ -10,16 +10,16 @@ const releaseObj = require(releaseJSON)
 const unpackagedVersionJSON = fs.readFileSync('build/electron/UnPackaged/version.json')
 const unpackagedVersionObj = JSON.parse(unpackagedVersionJSON)
 const content = {
-    "packageVer": "",
-    "channel": "",
-    "buildTime": "",
-    "buildCommit": "",
-    "sourceCommit": "",
-    "version": ""
+    packageVer: '',
+    channel: '',
+    buildTime: '',
+    buildCommit: '',
+    sourceCommit: '',
+    version: ''
 }
 const getBuildTime = async () => {
   return new Promise((resolve, reject) => {
-    exec('git log -1 --date=format:"%Y%m%d%H%M" --format="%cd"', (error, stdout, stderr) => {
+    exec('set TZ=UTC-8 && git log -1 --date=format-local:"%Y%m%d%H%M" --format="%cd"', (error, stdout, stderr) => {
     if (error) {
       console.error(`exec error: ${error}`)
       return
@@ -43,8 +43,24 @@ const getCommit = async () => {
         console.error(`Error from Git: ${stderr}`)
         return
       }
-      resolve(stdout.trim())
+    resolve(stdout.trim())
+    })
   })
+}
+const getSourceCommit = async () => {
+  return new Promise((resolve, reject) => {
+    exec('git log -1 --pretty=%B --author=Alphabiz-Team', (error, stdout, stderr) => {
+      if (error) {
+        console.error(`exec error: ${error}`)
+        return
+      }
+      if (stderr) {
+        console.error(`Error from Git: ${stderr}`)
+        return
+      }
+    const sha7 = /(?<=-)\w{7}(?=\s)|^\w{7}(?=\s)/.exec(stdout.trim())
+    resolve(sha7[0])
+    })
   })
 }
 const updateVersionJSON = async () => {
@@ -54,14 +70,15 @@ const updateVersionJSON = async () => {
   // 如果正式发布buildCommit 为触发正式发布的sha7
   if (!process.argv[3]) content.buildCommit = await getCommit()
   else content.buildCommit = process.argv[3]
-  content.sourceCommit = unpackagedVersionObj.sourceCommit
+  content.sourceCommit = await getSourceCommit()
   
   // 如果正式发布,则从argv传入新的tagname format: node update-version.js [newTagName] [newSHA7]
   if (!process.argv[2]){
     content.channel = 'nightly'
     content.version = content.packageVer + '-' + content.channel + '-' + content.buildTime
   } else {
-    content.channel = 'release'
+    if (process.argv.includes('--stable')) content.channel = 'stable'
+    else content.channel = 'nightly'
     content.version = process.argv[2]
   }
   
