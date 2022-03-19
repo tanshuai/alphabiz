a598f42bd0fe025d4aa985cd6b332da6f261fde8
zyl@alpha.biz <92558550+alphabiz-se4@users.noreply.github.com>
2022-03-19 14:30:56 +0800

    try test msi

--- test.config.js
+++ test.config.js
@@ -1,7 +1,7 @@
 const path = require('path')
 
 const AlphabizAppPath = path.resolve(__dirname, 'Alphabiz-win32-x64/Alphabiz.exe')
-
+const AlphabizAppMSIPath = 'C:/Program\\ Files/Alphabiz/Alphabiz.exe'
 const electronMainPath = 'build/electron/UnPackaged/electron-main.js'
 
-module.exports = { electronMainPath, AlphabizAppPath }
+module.exports = { electronMainPath, AlphabizAppPath, AlphabizAppMSIPath }
