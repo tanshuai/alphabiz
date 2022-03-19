6e212f2e26c8021675e942c277c0a012454f2cd3
zyl@alpha.biz <92558550+alphabiz-se4@users.noreply.github.com>
2022-03-19 16:00:40 +0800

    run test .exe

--- test/jest/release/TestEnvironment.js
+++ test/jest/release/TestEnvironment.js
@@ -3,9 +3,13 @@ let AlphabizAppPath
 if (process.env.APP_TYPE === 'msi') {
   AlphabizAppPath = require('../../../test.config.js').AlphabizAppMSIPath
   console.log('APP_TYPE = msi,AlphabizAppPath: ' + AlphabizAppPath)
-} else {
+} else if (process.env.APP_TYPE === 'exe') {
+  AlphabizAppPath = require('../../../test.config.js').AlphabizAppEXEPath
+  console.log('APP_TYPE = 7z,AlphabizAppPath: ' + AlphabizAppPath)
+}
+else {
   AlphabizAppPath = require('../../../test.config.js').AlphabizAppPath
-  console.log('APP_TYPE = exe,AlphabizAppPath: ' + AlphabizAppPath)
+  console.log('APP_TYPE = 7z,AlphabizAppPath: ' + AlphabizAppPath)
 }
 // const AlphabizAppPath = path.resolve(__dirname, '../../../dist/electron/Alphabiz-win32-x64/Alphabiz.exe')
 export const opts = {
