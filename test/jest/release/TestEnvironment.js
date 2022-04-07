fb5f75cfa2669485bc9f6ffd6e76e94972c3b79a
Alphabiz-Test-Team <git@alpha.biz>
2022-04-07 03:09:09 +0000

    afcbe0e update e2e test

--- test/jest/release/TestEnvironment.js
+++ test/jest/release/TestEnvironment.js
@@ -18,8 +18,8 @@ export const opts = {
     browserName: '',
     platformName: 'Windows',
     deviceName: 'WindowsPC',
-    appWaitDuration: 60000,
-    createSessionTimeout: 60000,
+    appWaitDuration: 20000,
+    createSessionTimeout: 20000,
     app: AlphabizAppPath
   },
   logLevel: 'error'
