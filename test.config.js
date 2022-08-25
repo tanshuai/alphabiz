51fab473a1737039a4257ba33a7684ddc53b589d
zyl@alpha.biz <92558550+alphabiz-se4@users.noreply.github.com>
2022-08-25 09:08:29 +0800

    run test .exe

--- test.config.js
+++ test.config.js
@@ -1,7 +1,8 @@
 const path = require('path')
 
 const AlphabizAppPath = path.resolve(__dirname, 'Alphabiz-win32-x64/Alphabiz.exe')
+const AlphabizAppEXEPath = (process.env.HOME + '/AppData/Local/Alphabiz/Alphabiz.exe').replace(/\\/g, '/')
 const AlphabizAppMSIPath = 'C:/Program Files/Alphabiz/Alphabiz.exe'
 const electronMainPath = 'build/electron/UnPackaged/electron-main.js'
 
-module.exports = { electronMainPath, AlphabizAppPath, AlphabizAppMSIPath }
+module.exports = { electronMainPath, AlphabizAppPath, AlphabizAppMSIPath, AlphabizAppEXEPath }
