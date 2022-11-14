3f11fa79678417e83e321b6a831c7e2beaf9e631
zyl <92558550+alphabiz-se4@users.noreply.github.com>
2022-11-14 15:40:01 +0800

    describe is limited to 200 characters

--- publicTwitter.js
+++ publicTwitter.js
@@ -2,7 +2,21 @@ const { TwitterApi } = require('twitter-api-v2')
 const fs = require('fs')
 require('dotenv').config()
 const describe = fs.readFileSync('./github-describe/github-describe.txt', 'utf-8')
-console.log(describe)
+
+// describe length cannot exceed 200
+const getDescribe = (describe) => {
+  let desc = describe.replace(/(,\s|)skip e2e/gm, '').replace(/^\s*\n/gm, '')
+  // delete duplicate lines
+  desc = desc.split('\r')
+    .filter((item, i, allItems) => {
+      return i === allItems.indexOf(item)
+    })
+    .join("\n")
+  desc = desc.substring(0, 200)
+  desc = desc.replace(/\r?\n?[^\r\n]*$/, "")
+  return desc
+}
+
 const publicTwitter = async (describe) => {
   const twitterClient = new TwitterApi({
     appKey: process.env.TWITTER_CONSUMER_KEY,
@@ -13,7 +27,9 @@ const publicTwitter = async (describe) => {
   await twitterClient.v2.tweet(describe)
   console.log('Tweet successfully!')
 }
-publicTwitter(describe)
+const desc = getDescribe(describe)
+console.log(desc)
+publicTwitter(desc)
 
 
 
