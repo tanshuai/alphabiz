700927636ae8488a149664452b2eebfd591e461e
Alphabiz-Test-Team <git@alpha.biz>
2023-01-05 02:08:56 +0000

    f9688be update e2e test

--- test/playwright/models/basicPage.js
+++ test/playwright/models/basicPage.js
@@ -10,18 +10,21 @@ class BasicPage extends BasePage {
     this.showExploreChk = page.locator('[aria-label="Show\ \[Explore\]\ page"]')
     this.i18n = {
       EN: {
+        languageText: page.locator('.setting-block:has-text("language") .setting-label span'),
         languageSelect: page.locator('.setting-block:has-text("language") label'),
         languageListBtn: page.locator('[role="listbox"] >> text=english'),
         saveBtn: page.locator('button:has-text("Save & Apply")'),
         saveAlert: page.locator('[role="alert"]:has-text("Save preferences successfully!")')
       },
       CN: {
+        languageText: page.locator('.setting-block:has-text("语言") .setting-label span'),
         languageSelect: page.locator('.setting-block:has-text("语言") label'),
         languageListBtn: page.locator('[role="listbox"] >> text=简体中文'),
         saveBtn: page.locator('button:has-text("保存并应用")'),
         saveAlert: page.locator('[role="alert"]:has-text("偏好设置成功")')
       },
       TW: {
+        languageText: page.locator('.setting-block:has-text("語言") .setting-label span'),
         languageSelect: page.locator('.setting-block:has-text("語言") label'), 
         languageListBtn: page.locator('[role="listbox"] >> text=繁體中文'),
         saveBtn: page.locator('button:has-text("保存並應用")'),
@@ -40,6 +43,7 @@ class BasicPage extends BasePage {
     if (startLanguage === 'EN') basicPage = 'basicLink'
     else basicPage = 'basicLink_' + startLanguage
     await this.jumpPage(basicPage)
+    await this.i18n[startLanguage].languageText.click({ force: true })
     await this.i18n[startLanguage].languageSelect.click()
     await this.i18n[targetLanguage].languageListBtn.click()
     await this.i18n[targetLanguage].saveBtn.click()
