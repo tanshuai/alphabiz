04130118c83c58bacdf151c00b161c023014e9bd
Alphabiz-Team <git@alpha.biz>
2023-02-20 10:37:47 +0000

    904826a update build-scripts resources

---
+++ build-scripts/common/shasum.js
@@ -0,0 +1,44 @@
+#!/usr/bin/env node
+
+const { existsSync, readdirSync, writeFileSync } = require('fs')
+const { resolve } = require('path')
+const getShasum = require('../../src-electron/utils/shasum')
+
+/**
+ * @function generateShasums
+ * @param { import('fs').PathLike } dir
+ */
+const generateShasums = async dir => {
+  const files = readdirSync(dir).filter(i => !i.endsWith('.sha'))
+  let count = 0
+  for (const file of files) {
+    const p = resolve(dir, file)
+    const sha = await getShasum(p)
+    if (typeof sha === 'string') {
+      /**
+       * The module `sumchecker` requires using `SHASUM256.txt` with following
+       * string format. This may be used in the future.
+       * @see https://www.npmjs.com/package/sumchecker
+       * @see https://en.wikipedia.org/wiki/Sha1sum
+       */
+      console.log(`${sha} *${file}`)
+      writeFileSync(p + '.sha', sha)
+      count += 1
+    } else {
+      console.warn(`Failed to generate shasum file for ${file}`)
+    }
+  }
+  console.log(`Generated ${count} shasum files`)
+}
+
+// Check if is command line call
+if (require.main === module) {
+  const dir = process.argv[2]
+  if (dir && existsSync(dir)) {
+    generateShasums(dir)
+  } else {
+    console.log('Require passing a directory with output installers')
+  }
+}
+
+module.exports = generateShasums
