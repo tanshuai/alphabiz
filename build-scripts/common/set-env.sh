618ee5c7f6005ed38c61a02c158544ab0af3d54f
Alphabiz-Team <git@alpha.biz>
2023-02-25 00:43:26 +0000

    042165d update build-scripts resources

--- build-scripts/common/set-env.sh
+++ build-scripts/common/set-env.sh
@@ -1,10 +1,10 @@
-#!/bin/sh
+#!/bin/bash
 # Set environments
 set -e # Exit when error
 
 function ensureExists () {
   if [ ! -e "$1" ]; then
-    echo "Error: "$1" does not exists. Make sure you have already craeted or built."
+    echo "Error: ""$1"" does not exists. Make sure you have already craeted or built."
     if [[ -z "$2" ]]; then
       echo "  $2"
     fi
@@ -13,15 +13,16 @@ function ensureExists () {
 }
 
 ENV_FILE=$(dirname "$(realpath "$0/../../..")")/.env
-if test -f $ENV_FILE; then
+if test -f "$ENV_FILE"; then
   echo "Loaded .env file"
+  # shellcheck source=../../.env
   source "$ENV_FILE"
 fi
 
 function ensureEnv () {
   arg=$1
   if [[ -z "${!arg}" ]]; then
-    echo "\nError: Env $arg not set."
+    echo "Error: Env $arg not set."
     if [[ -z "$2" ]]; then
       echo "  Set it before run your build arg"
     else
@@ -36,6 +37,7 @@ function ensureEnv () {
 
 IS_DEV=false
 if [[ $DEV = "true" ]]; then
+  # shellcheck disable=SC2034
   IS_DEV=true
   echo "Run in development mode."
   echo "Build logs will print to screen."
--- build-scripts/macos/dmg/build.sh
+++ build-scripts/macos/dmg/build.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 # Build .dmg targets for arm64 and x86_64
-source "$(realpath "$0/../../../common/set-env.sh")"
+# shellcheck source=../../common/set-env.sh
+. "$(realpath "$0/../../../common/set-env.sh")"
 
 echo "Preparing build for macos .dmg target..."
-if [ -z $BUILD_PLATFORM ]; then
+if [ -z "$BUILD_PLATFORM" ]; then
   echo "If you want to build the mas(Mac App Store) package, you need to add BUILD_PLATFORM=mas before you build apps."
   BUILD_PLATFORM="darwin"
 else
@@ -16,25 +17,26 @@ cd "$(dirname "$(realpath "$0/../../..")")"
 
 sleep 1
 
-echo "\n\nMaking dmg..."
+echo "Making dmg..."
 sleep 1
 
 ensureExists "dist/electron/$APP-$BUILD_PLATFORM-x64/$APP.app"
-echo "\x1b[32m  Runing \x1b[36m BUILD_ARCH=x64 BUILD_PLATFORM=$BUILD_PLATFORM yarn make \x1b[32m This may take minutes\x1b[0m"
+printf "\x1b[32m  Runing \x1b[36m BUILD_ARCH=x64 BUILD_PLATFORM=%1 yarn make \x1b[32m This may take minutes\x1b[0m\n" "$BUILD_PLATFORM"
 BUILD_ARCH=x64 BUILD_PLATFORM=$BUILD_PLATFORM yarn make
 echo "Successfully make x64 dmg"
 sleep 1
 
 ensureExists "dist/electron/$APP-$BUILD_PLATFORM-arm64/$APP.app"
-echo "\x1b[32m  Runing \x1b[36m BUILD_ARCH=arm64 BUILD_PLATFORM=$BUILD_PLATFORM yarn make \x1b[32m This may take minutes\x1b[0m"
+printf "\x1b[32m  Runing \x1b[36m BUILD_ARCH=arm64 BUILD_PLATFORM=%1 yarn make \x1b[32m This may take minutes\x1b[0m\n" "$BUILD_PLATFORM"
 BUILD_ARCH=arm64 BUILD_PLATFORM=$BUILD_PLATFORM yarn make
 echo "Successfully make arm64 dmg"
 sleep 1
 
 ensureExists "dist/electron/$APP-$BUILD_PLATFORM-universal/$APP.app"
-echo "\x1b[32m  Runing \x1b[36m BUILD_ARCH=universal BUILD_PLATFORM=$BUILD_PLATFORM yarn make \x1b[32m This may take minutes\x1b[0m"
+printf "\x1b[32m  Runing \x1b[36m BUILD_ARCH=universal BUILD_PLATFORM=%1 yarn make \x1b[32m This may take minutes\x1b[0m\n" "$BUILD_PLATFORM"
 BUILD_ARCH=universal BUILD_PLATFORM=$BUILD_PLATFORM yarn make
 echo "Successfully make universal dmg"
 sleep 1
 
-echo "\n\nDone.\n"
+echo "Done."
+echo ""
