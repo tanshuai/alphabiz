09e812b91bc0d689a99659ef553ddc5a9d8a9be7
Alphabiz-Test-Team <git@alpha.biz>
2023-03-04 10:37:30 +0000

    f40185b update e2e test

--- test/scripts/EC2.js
+++ test/scripts/EC2.js
@@ -40,6 +40,84 @@ function stopInstances () {
   })
 }
 
+//https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/EC2.html#allocateHosts-property
+function allocateHosts (params) {
+  return new Promise((resolve, reject) => {
+    const params = {
+      AvailabilityZone: 'us-east-1b', /* required */
+      Quantity: 1, /* required */
+      InstanceFamily: 'mac2',
+      InstanceType: 'mac2.metal'
+    }
+    ec2.allocateHosts(params, function(err, data) {
+      if (err) {
+        console.log(err, err.stack); // an error occurred
+        reject(err)
+      } else {
+        console.log(data)           // successful response
+        resolve(data)
+      }
+    })
+  })
+}
+
+//https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/EC2.html#modifyInstancePlacement-property
+function modifyInstancePlacement (param) {
+  return new Promise((resolve, reject) => {
+    const params = {
+      InstanceId: 'STRING_VALUE', /* required */
+      GroupId: 'STRING_VALUE',
+      GroupName: 'STRING_VALUE',
+      HostId: 'STRING_VALUE',
+      HostResourceGroupArn: 'STRING_VALUE',
+      PartitionNumber: 'NUMBER_VALUE',
+      Tenancy: dedicated | host
+    }
+    ec2.modifyInstancePlacement(params, function(err, data) {
+      if (err) {
+        console.log(err, err.stack); // an error occurred
+        reject(err)
+      } else {
+        console.log(data)           // successful response
+        resolve(data)
+      }
+    })
+  })
+}
+
+//https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/EC2.html#describeHosts-property
+function describeHosts (params) {
+  return new Promise((resolve, reject) => {
+    ec2.describeHosts(params, function(err, data) {
+      if (err) { // an error occurred
+        console.log(err, err.stack)
+        reject(err)
+      } else {  // successful response
+        console.log(data)
+        resolve(data?.Hosts)
+      }           
+    })
+  })
+}
+
+//https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/EC2.html#releaseHosts-property
+function releaseHost (HostIds) {
+  return new Promise((resolve, reject) => {
+    const params = {
+      HostIds: Array.isArray(HostIds) ? [...HostIds] : [HostIds]
+    };
+    ec2.releaseHosts(params, function(err, data) {
+      if (err) { // an error occurred
+        console.log(err, err.stack)
+        reject(err)
+      } else {  // successful response
+        // console.log(data)
+        resolve(data)
+      }    
+    })
+  })
+}
+
 async function start () {
   console.log(`\x1b[36m Start Instances... \x1b[0m`)
   await startInstances()
@@ -51,8 +129,49 @@ async function stop () {
   console.log('result', result)
 }
 
+async function allocate () {
+
+}
+
+async function release () {
+  // Describes all dedicated hosts
+  console.log(`\x1b[36m Describe Hosts... \x1b[0m`)
+  const HostsArr = await describeHosts({
+    Filter: [
+      {
+        Name: 'instance-type',
+        Values: [
+          'mac2.metal',
+        ]
+      },{
+        Name: 'state',
+        Values: [
+          'pending',
+          'failed ',
+          'available',
+          'unavailable'
+        ]
+      }
+    ]
+  })
+  console.log('HostsArr!', HostsArr)
+
+  if (!Array.isArray(HostsArr) || !HostsArr.length) return
+  const HostsIdArr = HostsArr.map((currentValue) => {
+    return currentValue.HostId
+  })
+  console.log('HostsIdArr!', HostsIdArr)
+
+  console.log(`\x1b[36m Release Host... \x1b[0m`)
+  const releaseHostResult = await releaseHost(HostsIdArr)
+  console.log('releaseHostResult!', releaseHostResult)
+  if (releaseHostResult.Unsuccessful.length) throw error('Release Host Unsuccessful!')
+}
+
 if (process.argv.includes('--start')) {
   start()
 } else if (process.argv.includes('--stop')) {
   stop()
+} else if (process.argv.includes('--release')) {
+  release()
 }
