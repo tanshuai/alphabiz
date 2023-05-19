728af37ed96e3f4583cbf62542aba8aaf40472ce
Alphabiz-Test-Team <git@alpha.biz>
2023-05-19 10:43:03 +0000

    4738540 update e2e test

--- test/playwright/models/oauthPage.js
+++ test/playwright/models/oauthPage.js
@@ -1,6 +1,6 @@
 const { expect } = require('@playwright/test')
 const { BasePage } = require('./basePage')
-
+const { getMailCode } = require('../../utils/getCode')
 class OauthPage extends BasePage {
   constructor (page) {
     super(page)
@@ -14,13 +14,21 @@ class OauthPage extends BasePage {
   }
 
   async signInGithub (username, password) {
+    let newTime
     await this.page.waitForTimeout(3000)
     if (await this.page.locator('[name="login"]').isVisible()) {
       await this.page.locator('[name="login"]').fill(username)
       await this.page.locator('[name="password"]').fill(password)
+      newTime = new Date()
       await this.page.locator('[name="commit"]').click()
     }
     await this.page.waitForTimeout(3000)
+    if (await this.page.locator('input[placeholder="XXXXXX"]').isVisible()) {
+      const verificationCode = await getMailCode({ type: 1, time: newTime, to: username, oauth: 'github' })
+      await this.page.locator('input[placeholder="XXXXXX"]').fill(verificationCode)
+      await this.page.locator('input[value="Verify"]').click()
+      await this.page.waitForTimeout(5000)
+    }
     if (await this.page.locator('button:has-text("Authorize alphabiz")').isVisible()) {
       await this.page.waitForTimeout(5000)
       await this.page.locator('button:has-text("Authorize alphabiz")').click()
@@ -28,12 +36,14 @@ class OauthPage extends BasePage {
   }
 
   async signInTwitter (username, password) {
+    let newTime
     await this.page.waitForURL('https://twitter.com/i/oauth2/**')
     await this.page.waitForTimeout(3000)
     if (await this.page.locator('[data-testid="OAuth_Consent_Log_In_Button"]').isVisible()) {
       await this.page.locator('[data-testid="OAuth_Consent_Log_In_Button"]').click()
       await this.page.locator('input[autocomplete="username"]').fill(username)
       await this.page.waitForTimeout(3000)
+      newTime = new Date()
       await this.page.locator('div[role="button"]:has-text("Next")').click()
       await this.page.waitForTimeout(3000)
       // if (await this.page.locator('[data-testid="ocfEnterTextTextInput"]').isVisible()) {
@@ -44,10 +54,17 @@ class OauthPage extends BasePage {
       if (await this.page.locator('input[name="password"]').isVisible()) {
         await this.page.locator('input[name="password"]').fill(password)
         await this.page.waitForTimeout(3000)
+        newTime = new Date()
         await this.page.locator('[data-testid="LoginForm_Login_Button"]').click()
       }
+      await this.page.waitForTimeout(5000)
+      if (await this.page.locator('text=Check your email').isVisible()) {
+        const verificationCode = await getMailCode({ type: 1, time: newTime, to: username, oauth: 'twitter' })
+        await this.page.locator('label:has-text("Confirmation code") input').fill(verificationCode)
+        await this.page.locator('div[role="button"]:has-text("Next")').click()
+        await this.page.waitForTimeout(5000)
+      }
     }
-    await this.page.waitForTimeout(5000)
     if (await this.page.locator('[data-testid="OAuth_Consent_Button"]').isVisible()) {
       await this.page.locator('[data-testid="OAuth_Consent_Button"]').click()
     }
