a2977af6ef922f37072619ad77ba3d660a8cb0f9
zyl <92558550+alphabiz-se4@users.noreply.github.com>
2023-08-09 10:00:27 +0800

    public describe to Twitter

---
+++ publicTwitter.js
@@ -0,0 +1,30 @@
+const Twitter = require('twitter')
+const fs = require('fs')
+require('dotenv').config()
+const describe = fs.readFileSync('./github-describe/github-describe.txt', 'utf-8')
+console.log(describe)
+const publicTwitter = (describe) => {
+  const client = new Twitter({
+    consumer_key: process.env.TWITTER_CONSUMER_KEY,
+    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+    access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
+  })
+  client.post(
+    'statuses/update',
+    { status: describe },
+    function (error, tweet, response) {
+      if (error) {
+        if (error && error[0].message && error[0].message.includes('duplicate.')) {
+          console.log('error:', error[0].message)
+          return
+        } else {
+          throw error
+        }
+      }
+      console.log(tweet.text); // Tweet body.
+      // console.log(response); // Raw response object.
+    }
+  )
+}
+// publicTwitter(describe)
