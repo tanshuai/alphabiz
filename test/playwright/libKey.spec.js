a66a208d867c8c266979639e0294481a618687e3
Alphabiz-Test-Team <git@alpha.biz>
2023-08-09 04:33:11 +0000

    9373306 update e2e test

--- test/playwright/libKey.spec.js
+++ test/playwright/libKey.spec.js
@@ -116,14 +116,23 @@ test.describe('librayKey:媒体库密钥测试', () => {
       await window.screenshot({ path: `${ScreenshotsPath}macos-disableCloudKey-screen.png` })
       await window.waitForTimeout(500)
       const count = await window.locator('.q-card:has-text("INTERNAL DEMO ONLY")').count()
-      if (count) await basePage.closeInternalNotice()
-    }
-    await accountPage.disableCloudKey()
-    if (process.platform === 'darwin') {
-      console.log("macos环境, 成功清除密钥")
+      if (count) {
+        await basePage.closeInternalNotice()
+        await console.log('关闭弹窗')
+      }
+      await accountPage.disableCloudKey()
+      await console.log("macos环境, 成功清除密钥")
       await window.screenshot({ path: `${ScreenshotsPath}macos-disableCloudKey-screen.png` })
+      await console.log('准备退出')
+      await basePage.signOut()
+      await window.screenshot({ path: `${ScreenshotsPath}macos-logout-screen.png` })
+      await console.log('退出')
     }
-    await basePage.signOut()
+    if (process.platform === 'darwin') {
+    } else{
+      await accountPage.disableCloudKey()
+      await basePage.signOut()
+    }
   })
   test.describe('独立密码', () => { 
     const inPassword = accountResetPassword
