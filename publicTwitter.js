171aa230a8a9d8bc95eae78a60e2f74b191bcfe9
zyl <92558550+alphabiz-se4@users.noreply.github.com>
2023-08-14 17:08:32 +0800

    tweet with new key

--- publicTwitter.js
+++ publicTwitter.js
@@ -1,30 +1,19 @@
-const Twitter = require('twitter')
+const { TwitterApi } = require('twitter-api-v2')
 const fs = require('fs')
 require('dotenv').config()
 const describe = fs.readFileSync('./github-describe/github-describe.txt', 'utf-8')
 console.log(describe)
-const publicTwitter = (describe) => {
-  const client = new Twitter({
-    consumer_key: process.env.TWITTER_CONSUMER_KEY,
-    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-    access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
-    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
+const publicTwitter = async (describe) => {
+  const twitterClient = new TwitterApi({
+    appKey: process.env.TWITTER_CONSUMER_KEY,
+    appSecret: process.env.TWITTER_CONSUMER_SECRET,
+    accessToken: process.env.TWITTER_ACCESS_TOKEN_KEY,
+    accessSecret: process.env.TWITTER_ACCESS_TOKEN_SECRET
   })
-  client.post(
-    'statuses/update',
-    { status: describe },
-    function (error, tweet, response) {
-      if (error) {
-        if (error && Array.isArray(error) && error[0].message && error[0].message.includes('duplicate.')) {
-          console.log('error:', error[0].message)
-          return
-        } else {
-          throw error
-        }
-      }
-      console.log(tweet.text); // Tweet body.
-      // console.log(response); // Raw response object.
-    }
-  )
+  await twitterClient.v2.tweet(describe)
+  console.log('Tweet successfully!')
 }
 // publicTwitter(describe)
+
+
+
