e45e10c7dc681d0614123a5bb20addcbc7f9f5d9
zyl <92558550+alphabiz-se4@users.noreply.github.com>
2023-08-31 16:31:48 +0800

    check last tweet content

--- publicTwitter.js
+++ publicTwitter.js
@@ -2,7 +2,7 @@ const { TwitterApi } = require('twitter-api-v2')
 const fs = require('fs')
 require('dotenv').config()
 let describe = fs.readFileSync('./github-describe/github-describe.txt', 'utf-8')
-describe = describe.replace(/\r\n/gm, '\r')
+describe = describe.replace(/\r\n/gm, '\n')
 // describe length cannot exceed 280
 const getDescribe = (describe) => {
   let desc = describe.replace(/(,\s|)skip e2e/gm, '').replace(/^\s*\n/gm, '')
@@ -18,10 +18,24 @@ const publicTwitter = async (describe) => {
     accessToken: process.env.TWITTER_ACCESS_TOKEN_KEY,
     accessSecret: process.env.TWITTER_ACCESS_TOKEN_SECRET
   })
+  const res = await twitterClient.v2.userTimeline('424858141')
+  const lastDesc = res._realData.data[0].text
+  const tweetList = res._realData.data
+
+  for (const tweetObj of tweetList) {
+    const lastDesc = tweetObj.text
+    // console.log(tweetObj)
+    // console.log('------------------------')
+    if (describe === lastDesc + '\n' || describe === lastDesc.replace(/\r/gm, '\n')) {
+      console.log('duplicate content!')
+      return false
+    }
+  }
   await twitterClient.v2.tweet(describe).catch(err => {
     if (!err.data.detail.includes('Tweet with duplicate content')) throw err
   })
   console.log('Tweet successfully!')
+  return true
 }
 
 function sleep (ms) {
@@ -42,7 +56,8 @@ function sleep (ms) {
   }
   console.log(descArr)
   for (const d of descArr) {
-    await publicTwitter(d)
+    const result = await publicTwitter(d)
+    if (!result) return
     await sleep(5000)
   }
 })()
