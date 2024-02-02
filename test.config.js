3f5ce1633d362941a1c5ffe61c57c8c5b6fdf053
zyl <92558550+alphabiz-se4@users.noreply.github.com>
2024-02-02 11:40:49 +0800

    fix release e2e test app path error

--- test.config.js
+++ test.config.js
@@ -3,7 +3,7 @@ const path = require('path')
 const appConfig = require('./developer/app')
 const productName = appConfig.displayName
 
-const AlphabizAppPath = path.resolve(__dirname, `dist/electron/${productName}-win32-x64/${productName}.exe`)
+const AlphabizAppPath = path.resolve(__dirname, `${productName}-win32-x64/${productName}.exe`)
 const AlphabizAppEXEPath = (process.env.HOME + `/AppData/Local/${productName}/${productName}.exe`).replace(/\\/g, '/')
 const AlphabizAppMSIPath = `C:/Program Files/${productName}/${productName}.exe`
 const electronMainPath = 'build/electron/UnPackaged/electron-main.js'
