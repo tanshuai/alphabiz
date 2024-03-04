7a930c0af6f718990cb8189e3165892f24ef7601
zyl <92558550+alphabiz-se4@users.noreply.github.com>
2024-03-04 10:37:43 +0800

    try packager

--- packager.js
+++ packager.js
@@ -7,6 +7,7 @@ const version = require('./package.json').version
 const publicVersion = require('./public/version.json').version
 
 const buildVersion = publicVersion || version
+const buildArch = process.env.BUILD_ARCH || process.arch
 
 const app = require('./developer/app')
 const appName = app.name
@@ -31,7 +32,7 @@ packageJson.productName = appName
 const beforeBuild = async () => {
   const { platform, arch } = process
   const destDir = path.resolve(__dirname, `build/electron/${displayName}-${platform}-${arch}`)
-  console.log('beforeBuild', destDir)
+  console.log('Before build', destDir)
   if (fs.existsSync(destDir)) {
     fs.rmSync(destDir, { recursive: true })
     console.log('删除成功！')
@@ -45,6 +46,9 @@ packager({
   name: app.displayName,
   appVersion: buildVersion,
   buildVersion: buildVersion,
+  arch: buildArch,
+  // `BUILD_PLATFORM=mas yarn build` for building Mac App Store target
+  platform: process.env.BUILD_PLATFORM || process.platform,
   extraResource: [
     path.resolve(__dirname, 'developer/icon-1024.png'),
     path.resolve(__dirname, 'developer/favicon.ico'),
@@ -60,7 +64,7 @@ packager({
   afterPrune: [(buildPath, electronVersion, platform, arch, callback) => {
     // console.log('---App Build Path---\n', buildPath)
     [
-      ...patches,
+      // ...patches,
       'torrent-discovery', // this builds with self-dep bittorrent-tracker
       '@videojs'
     ].forEach(dep => {
@@ -68,7 +72,24 @@ packager({
       const dest = path.resolve(buildPath, 'node_modules', dep)
       if (!fs.existsSync(src)) return console.error('not found', src)
       // console.log('--- COPY ---\n', src, '\n', dest, '\n--- COPY END ---')
-      if (fs.existsSync(dest)) fs.rmSync(dest, { recursive: true })
+      // fs.rmSync not support low version
+      const removeDir = (dir) => {
+        const files = fs.readdirSync(dir) // 一个数组
+        files.forEach((item) => {
+          let file = dir + "/" + item
+          if (fs.statSync(file).isFile()) {
+            fs.unlinkSync(file)
+          } else {
+            removeDir(file)
+          }
+        })
+        fs.rmdirSync(dir)
+      }
+      if (fs.existsSync(dest)) {
+        const nodeVersion = /(?<=v)\d{1,2}(?=.)/.exec(process.version)
+        if (nodeVersion[0] < 14) removeDir(dest)
+        else fs.rmSync(dest, { recursive: true })
+      }
       const copyRecursive = (src, dest) => {
         if (fs.statSync(src).isDirectory()) {
           fs.readdirSync(src).forEach(dir => {
@@ -84,49 +105,49 @@ packager({
       }
       copyRecursive(src, dest)
     })
-      ;[
-        'assets',
-        'icons',
-        'platform-assets',
-        '.'
-      ].forEach(assetDir => {
-        const src = path.resolve(__dirname, 'developer', assetDir)
-        const dest = path.resolve(buildPath, assetDir === '.' ? 'node_modules/developer' : 'developer/' + assetDir)
-        if (!fs.existsSync(src)) return console.error('not found', src)
-        // console.log('--- COPY ---\n', src, '\n', dest, '\n--- COPY END ---')
-        // fs.rmSync not support low version
-        const removeDir = (dir) => {
-          const files = fs.readdirSync(dir) // 一个数组
-          files.forEach((item) => {
-            let file = dir + "/" + item
-            if (fs.statSync(file).isFile()) {
-              fs.unlinkSync(file)
-            } else {
-              removeDir(file)
-            }
-          })
-          fs.rmdirSync(dir)
-        }
-        if (fs.existsSync(dest)) {
-          const nodeVersion = /(?<=v)\d{1,2}(?=.)/.exec(process.version)
-          if (nodeVersion[0] < 14) removeDir(dest)
-          else fs.rmSync(dest, { recursive: true })
-        }
-        const copyRecursive = (src, dest) => {
-          if (fs.statSync(src).isDirectory()) {
-            fs.readdirSync(src).forEach(dir => {
-              copyRecursive(path.resolve(src, dir), path.resolve(dest, dir))
-            })
+    ;[
+      'assets',
+      'icons',
+      'platform-assets',
+      '.'
+    ].forEach(assetDir => {
+      const src = path.resolve(__dirname, 'developer', assetDir)
+      const dest = path.resolve(buildPath, assetDir === '.' ? 'node_modules/developer' : 'developer/' + assetDir)
+      if (!fs.existsSync(src)) return console.error('not found', src)
+      // console.log('--- COPY ---\n', src, '\n', dest, '\n--- COPY END ---')
+      // fs.rmSync not support low version
+      const removeDir = (dir) => {
+        const files = fs.readdirSync(dir) // 一个数组
+        files.forEach((item) => {
+          let file = dir + "/" + item
+          if (fs.statSync(file).isFile()) {
+            fs.unlinkSync(file)
           } else {
-            // ensure directory exists
-            if (!fs.existsSync(path.dirname(dest))) {
-              fs.mkdirSync(path.dirname(dest), { recursive: true })
-            }
-            fs.copyFileSync(src, dest)
+            removeDir(file)
           }
+        })
+        fs.rmdirSync(dir)
+      }
+      if (fs.existsSync(dest)) {
+        const nodeVersion = /(?<=v)\d{1,2}(?=.)/.exec(process.version)
+        if (nodeVersion[0] < 14) removeDir(dest)
+        else fs.rmSync(dest, { recursive: true })
+      }
+      const copyRecursive = (src, dest) => {
+        if (fs.statSync(src).isDirectory()) {
+          fs.readdirSync(src).forEach(dir => {
+            copyRecursive(path.resolve(src, dir), path.resolve(dest, dir))
+          })
+        } else {
+          // ensure directory exists
+          if (!fs.existsSync(path.dirname(dest))) {
+            fs.mkdirSync(path.dirname(dest), { recursive: true })
+          }
+          fs.copyFileSync(src, dest)
         }
-        copyRecursive(src, dest)
-      })
+      }
+      copyRecursive(src, dest)
+    })
     const packageJsonPath = path.resolve(buildPath, 'package.json')
     fs.writeFileSync(packageJsonPath, fs.readFileSync(packageJsonPath).toString().replace(/Alphabiz/g, appName))
     const indexPath = path.resolve(buildPath, 'index.html')
@@ -137,13 +158,58 @@ packager({
     callback()
   }],
   afterCopy: [(buildPath, electronVersion, platform, arch, callback) => {
+    console.log('Copy patches to', buildPath)
+    fs.cpSync(resolve(__dirname, 'patches'), resolve(buildPath, 'patches'), { recursive: true })
+    const result = execSync('npx patch-package', { cwd: buildPath })
+    console.log(`Patch package result: ${result.toString()}`)
+    // const yarnResult = execSync('yarn install --production', { cwd: buildPath })
+    // console.log(`Yarn result: ${yarnResult.toString()}`)
+    if (platform === 'darwin') {
+      unsupportedModules.forEach(mod => {
+        const prebuildDir = resolve(buildPath, 'node_modules', mod, 'prebuilds')
+        if (fs.existsSync(prebuildDir)) {
+          console.log('[Darwin] Remove prebuilds directory for', mod)
+          fs.rmSync(prebuildDir, { recursive: true })
+        }
+        const buildDir = resolve(buildPath, 'node_modules', mod, 'build')
+        if (fs.existsSync(buildDir)) {
+          console.log('[Darwin] Remove build directory for', mod)
+          fs.rmSync(buildDir, { recursive: true })
+        }
+      })
+    }
+    const buildName = `${app.displayName}-${platform}-${buildArch}`
+    const basePath = buildPath.split(buildName)?.[0]
+    // #894 `node-gyp` throws errors when path includes spaces
+    const hasSpace = /\s/.test(buildName)
+    if (hasSpace) {
+      console.log(`The displayName "${app.displayName}" includes spaces. Use appName ${appName} for rebuild`)
+      fs.cpSync(resolve(basePath, buildName), resolve(basePath, appName), { recursive: true, verbatimSymlinks: true })
+    }
     rebuild({
-      buildPath,
+      buildPath: hasSpace ? buildPath.replace(buildName, appName) : buildPath,
       arch,
-      electronVersion: '17.0.0'
+      electronVersion
     })
       .then(() => {
-        console.log('Rebuilt native module')
+        console.log('Rebuilt native module', platform, arch)
+        if (hasSpace) {
+          console.log('Copy rebuild results to build path', buildPath)
+          fs.cpSync(resolve(basePath, appName), resolve(basePath, buildName), {
+            recursive: true,
+            verbatimSymlinks: true
+          })
+        }
+        const modules = fs.readdirSync(resolve(buildPath, 'node_modules'))
+        for (const module of modules) {
+          const moduleDir = resolve(buildPath, 'node_modules', module)
+          const releaseDir = resolve(moduleDir, 'build/Release')
+          if (fs.existsSync(releaseDir)) {
+            console.log(`Module [${module}] releases:`, fs.readdirSync(releaseDir))
+            pruneNative(resolve(moduleDir, 'build'))
+            pruneNative(resolve(moduleDir, 'node-addon-api'))
+          }
+        }
         callback()
       })
       .catch(e => callback(e))
