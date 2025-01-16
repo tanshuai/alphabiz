26e9afee2a7f16066fe1599b2fd22a30a96150b1
Alphabiz-Team <git@alpha.biz>
2025-01-16 16:13:13 +0800

    4738540

--- dist/spa/developer/dynamicConfig.js
+++ dist/spa/developer/dynamicConfig.js
@@ -1,1 +1,1 @@
-module.exports={remote:{url:"https://alpha.biz/app/remote_config.json"},local:{id:"local_config_v1",oauth:{enable:!1,providers:["Github","Twitter"]},update:{enable:!1}}};
+module.exports={remote:{url:"https://alpha.biz/app/remote_config.json"},local:{id:"local_config_v1",oauth:{enable:!1,providers:["Github","Twitter"]},library:{show_explore:!1},update:{enable:!1}}};
