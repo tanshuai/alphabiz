1d8fb0dd1cab8e8ab9543c7e7ebb27c5cc9e8409
alphabiz-se3 <hsn@alpha.biz>
2025-02-16 09:37:32 +0800

    add code-signing and notarization for macos pkg build

--- build-scripts/macos/app/buildEntitlements.js
+++ build-scripts/macos/app/buildEntitlements.js
@@ -8,14 +8,21 @@ const fullIdentifier = teamId + '.' + identifier
 
 const toReplace = { identifier, teamId, fullIdentifier }
 
-const entitlements = ['mas', 'inherit', 'loginhelper']
-
-const buildEntitlements = async (dist = '') => {
+const buildEntitlements = async (dist = '', isPkg = false) => {
+  const entitlements = ['mas', 'inherit', 'loginhelper']
+  if (isPkg) {
+    console.log('Is PKG')
+    entitlements[0] = ''
+  }
   console.log('build to', dist)
   entitlements.forEach(name => {
     console.log('Building', name)
-    const src = resolve(__dirname, `entitlements.${name}.plist`)
-    const dest = resolve(dist, `entitlements.${name}.plist`)
+    const src = name
+      ? resolve(__dirname, `entitlements.${name}.plist`)
+      : resolve(__dirname, `entitlements.plist`)
+    const dest = name ?
+      resolve(dist, `entitlements.${name}.plist`) :
+      resolve(dist, `entitlements.mas.plist`)
     let info = readFileSync(src, 'utf-8')
     for (const key in toReplace) {
       while (info.includes(`{{${key}}}`)) {
@@ -33,4 +40,4 @@ if (process.argv[2] && existsSync(process.argv[2])) {
   console.warn('Warn: dist not be specified or not exists.')
   dist = resolve(tmpdir(), 'electron-build/entitlements')
 }
-buildEntitlements(dist)
+buildEntitlements(dist, process.argv.includes('--pkg'))
