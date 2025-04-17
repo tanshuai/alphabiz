ee3bedefde610b605f556319615c2b3cbd54b764
Alphabiz-Team <git@alpha.biz>
2025-04-17 11:04:07 +0800

    823a94b update build-scripts resources

--- build-scripts/macos/pkg/sign.sh
+++ build-scripts/macos/pkg/sign.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
-# Sign .app for releasing
 # shellcheck source=../../common/set-env.sh
-. "build-scripts/common/set-env.sh"
+# Sign .app for normal release
+# This script will sign darwin app for non-store builds.
+# Note that different from the `sign.sh` in macos/app which signs MAS build,
+# this script will sign app with hardened runtime which is required by apple
+# notarization service.
+# The app signed by this script is invalid for MAS submission.
 
+. "build-scripts/common/set-env.sh"
 
 ensureEnv "APP" "your app name"
 ensureEnv "VERSION" "x.x.x"
@@ -10,21 +15,13 @@ ensureEnv "VERSION" "x.x.x"
 ensureEnv "PLATFORM" "\"mas\""
 ensureEnv "APPLE_TEAM_ID" "org.your-org.app-name"
 
-# ARM_PATH="out/$APP-$PLATFORM-arm64/$APP.app"
-# X64_PATH="out/$APP-$PLATFORM-x64/$APP.app"
 ensureEnv "BUILD_ARCH"
-UNV_PATH="out/$APP-$PLATFORM-$BUILD_ARCH/$APP.app"
-# if $IS_DEV; then
-#   echo "DEV $IS_DEV"
-#   DEBUG="electron-osx-sign"
-# else
-#   DEBUG=""
-# fi
+APP_PATH="dist/electron/$APP-$PLATFORM-$BUILD_ARCH/$APP.app"
 
-ensureEnv "APPLE_DISTRIBUTION_KEY" "\"Apple Development: Company Name (XXXXXXXXXX)\""
+ensureEnv "APPLE_DISTRIBUTION_KEY" "\"Developer ID Distribution: Company Name (XXXXXXXXXX)\""
 
 if [ -e "developer/embedded.provisionprofile" ]; then
-  cp "developer/embedded.provisionprofile" "$UNV_PATH/Contents/"
+  cp "developer/embedded.provisionprofile" "$APP_PATH/Contents/"
   echo "Added embedded provisoning profile"
 else
   echo "Warn: Cannot find embedded.provisionprofile in developer folder. This may cause error if you submit signed app to MAS"
@@ -52,40 +49,28 @@ LOGINHELPER="$ENTITLEMENTS_DIR/entitlements.loginhelper.plist"
 cat "$ENTITLEMENT"
 echo ""
 
-echo "Start signing"
-echo ""
+echo "Start signing..."
 
-find "$UNV_PATH" -name "* Framework" -exec codesign -s "$APPLE_DISTRIBUTION_KEY" --timestamp -f --options runtime --entitlements "$INHERIT" "{}" \;
-find "$UNV_PATH" -name "*.dylib" -exec codesign -s "$APPLE_DISTRIBUTION_KEY" --timestamp -f --entitlements "$INHERIT" "{}" \;
-find "$UNV_PATH" -name "*.framework" -exec codesign -s "$APPLE_DISTRIBUTION_KEY" --timestamp -f --options runtime --entitlements "$INHERIT" "{}" \;
-find "$UNV_PATH" -name "*.node" -exec codesign -s "$APPLE_DISTRIBUTION_KEY" --timestamp -f --entitlements "$INHERIT" "{}" \;
-echo "Signed frameworks"
+# The binary files should be added first
+find "$APP_PATH" -name "* Framework" -exec codesign -s "$APPLE_DISTRIBUTION_KEY" --timestamp -f --options runtime --entitlements "$INHERIT" "{}" \;
+find "$APP_PATH" -name "*.dylib" -exec codesign -s "$APPLE_DISTRIBUTION_KEY" --timestamp -f --entitlements "$INHERIT" "{}" \;
+find "$APP_PATH" -name "*.framework" -exec codesign -s "$APPLE_DISTRIBUTION_KEY" --timestamp -f --options runtime --entitlements "$INHERIT" "{}" \;
+find "$APP_PATH" -name "*.node" -exec codesign -s "$APPLE_DISTRIBUTION_KEY" --timestamp -f --entitlements "$INHERIT" "{}" \;
 
-# codesign -s "$APPLE_DISTRIBUTION_KEY" --timestamp -f --options runtime --entitlements "$INHERIT" "$UNV_PATH/Contents/Frameworks/$APP Helper.app/Contents/MacOS/$APP Helper"
-# codesign -s "$APPLE_DISTRIBUTION_KEY" --timestamp -f --options runtime --entitlements "$INHERIT" "$UNV_PATH/Contents/Frameworks/$APP Helper.app/"
-# codesign -s "$APPLE_DISTRIBUTION_KEY" --timestamp -f --options runtime --entitlements "$INHERIT" "$UNV_PATH/Contents/Frameworks/$APP Helper (Renderer).app/Contents/MacOS/$APP Helper (Renderer)"
-# codesign -s "$APPLE_DISTRIBUTION_KEY" --timestamp -f --options runtime --entitlements "$INHERIT" "$UNV_PATH/Contents/Frameworks/$APP Helper (Renderer).app/"
-# codesign -s "$APPLE_DISTRIBUTION_KEY" --timestamp -f --options runtime --entitlements "$INHERIT" "$UNV_PATH/Contents/Frameworks/$APP Helper (GPU).app/Contents/MacOS/$APP Helper (GPU)"
-# codesign -s "$APPLE_DISTRIBUTION_KEY" --timestamp -f --options runtime --entitlements "$INHERIT" "$UNV_PATH/Contents/Frameworks/$APP Helper (GPU).app/"
-# codesign -s "$APPLE_DISTRIBUTION_KEY" --timestamp -f --options runtime --entitlements "$INHERIT" "$UNV_PATH/Contents/Frameworks/$APP Helper (Plugin).app/Contents/MacOS/$APP Helper (Plugin)"
-# codesign -s "$APPLE_DISTRIBUTION_KEY" --timestamp -f --options runtime --entitlements "$INHERIT" "$UNV_PATH/Contents/Frameworks/$APP Helper (Plugin).app/"
-if [ -e "$UNV_PATH/Contents/Library" ]; then
-  codesign -s "$APPLE_DISTRIBUTION_KEY" --timestamp -f --options runtime --entitlements "$LOGINHELPER" "$UNV_PATH/Contents/Library/LoginItems/$APP Login Helper.app/Contents/MacOS/$APP Login Helper"
-  codesign -s "$APPLE_DISTRIBUTION_KEY" --timestamp -f --options runtime --entitlements "$LOGINHELPER" "$UNV_PATH/Contents/Library/LoginItems/$APP Login Helper.app"
+if [ -e "$APP_PATH/Contents/Library" ]; then
+  # This is only available in mas build
+  codesign -s "$APPLE_DISTRIBUTION_KEY" --timestamp -f --options runtime --entitlements "$LOGINHELPER" "$APP_PATH/Contents/Library/LoginItems/$APP Login Helper.app/Contents/MacOS/$APP Login Helper"
+  codesign -s "$APPLE_DISTRIBUTION_KEY" --timestamp -f --options runtime --entitlements "$LOGINHELPER" "$APP_PATH/Contents/Library/LoginItems/$APP Login Helper.app"
 fi
-if [ -e "$UNV_PATH/Contents/Frameworks/Squirrel.framework/Versions/A/Resources/ShipIt" ]; then
+
+# The ShipIt binary file in Squirrel framework requires signing directly
+if [ -e "$APP_PATH/Contents/Frameworks/Squirrel.framework/Versions/A/Resources/ShipIt" ]; then
   # This is only available in non-mas build
-  codesign -s "$APPLE_DISTRIBUTION_KEY" --timestamp -f --options runtime --entitlements "$INHERIT" "$UNV_PATH/Contents/Frameworks/Squirrel.framework/Versions/A/Resources/ShipIt"
+  codesign -s "$APPLE_DISTRIBUTION_KEY" --timestamp -f --options runtime --entitlements "$INHERIT" "$APP_PATH/Contents/Frameworks/Squirrel.framework/Versions/A/Resources/ShipIt"
 fi
-# if [ -e "$UNV_PATH/Contents/Frameworks/Squirrel.framework/Versions/A/Squirrel" ]; then
-#   codesign -s "$APPLE_DISTRIBUTION_KEY" --timestamp -f --options runtime --entitlements "$INHERIT" "$UNV_PATH/Contents/Frameworks/Squirrel.framework/Versions/A/Squirrel"
-# fi
 
-echo "Sign .app"
-echo ""
-# # find -f "$UNV_PATH" -exec sh -c codesign -s "$APPLE_DISTRIBUTION_KEY" --timestamp --entitlements "$INHERIT" -f --deep "$UNV_PATH" \;
-# codesign -s "$APPLE_DISTRIBUTION_KEY" --timestamp --options runtime --entitlements "$ENTITLEMENT" -f "$UNV_PATH/Contents/MacOS/$APP"
-codesign -s "$APPLE_DISTRIBUTION_KEY" --deep --timestamp --options runtime --entitlements "$ENTITLEMENT" -f "$UNV_PATH"
+# Use --deep for signing all other files
+codesign -s "$APPLE_DISTRIBUTION_KEY" --deep --timestamp --options runtime --entitlements "$ENTITLEMENT" -f "$APP_PATH"
 
 sleep 1
-printf "Done\n"
+echo "Finish signing $APP_PATH"
