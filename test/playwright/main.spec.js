f403c346839662c320aee1f5e07f8933efd6db52
Alphabiz-Test-Team <git@alpha.biz>
2026-05-30 13:43:11 +0800

    8851902 update e2e test

--- test/playwright/main.spec.js
+++ test/playwright/main.spec.js
@@ -366,6 +366,9 @@ test.describe('切换语言设置', () => {
     })
     // EN -> CN -> TW -> EN
     test('语言重复切换-EN-CN-TW-EN', async () => {
+      if (process.platform === 'linux'){
+        test.skip()
+      }
       // 确保语言en
       await basePage.clearLocalstorage()
       await window.waitForTimeout(3000)
